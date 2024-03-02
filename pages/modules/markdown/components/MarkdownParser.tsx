c1e5a38785fa30e6fb8b9fe891f80633c9e894b6
Pie :3! <94876844+PiCake314@users.noreply.github.com>
2024-03-02 13:56:25 -0700

    added syntax highlighting

--- pages/modules/markdown/components/MarkdownParser.tsx
+++ pages/modules/markdown/components/MarkdownParser.tsx
@@ -2,12 +2,80 @@ import { markdownParserProps } from '../types/markdownParser';
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
+import { FC } from 'react';
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
+// import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
+// import scss from 'react-syntax-highlighter/dist/cjs/languages/prism/scss';
+// import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
+// import markdown from 'react-syntax-highlighter/dist/cjs/languages/prism/markdown';
+// import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
+
+SyntaxHighlighter.registerLanguage('tsx', tsx);
+// SyntaxHighlighter.registerLanguage('typescript', typescript);
+// SyntaxHighlighter.registerLanguage('scss', scss);
+// SyntaxHighlighter.registerLanguage('bash', bash);
+// SyntaxHighlighter.registerLanguage('markdown', markdown);
+// SyntaxHighlighter.registerLanguage('json', json);
+
+
+import rangeParser from 'parse-numeric-range';
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+type MarkdownProps = {
+  markdown: string & { content?: string };
+};
+
 export default function MarkdownParser({token}: markdownParserProps): JSX.Element {
+
+  const syntaxTheme = oneDark;
+
+  const MarkdownComponents: object = {
+    code({ node, inline, className, ...props }) {
+      const hasLang = /language-(\w+)/.exec(className || '');
+      const hasMeta = node?.data?.meta;
+
+      const applyHighlights: object = (applyHighlights: number) => {
+        if (hasMeta) {
+          const RE = /{([\d,-]+)}/;
+          const metadata = node.data.meta?.replace(/\s/g, '');
+          const strlineNumbers = RE?.test(metadata)
+            ? RE?.exec(metadata)[1]
+            : '0';
+          const highlightLines = rangeParser(strlineNumbers);
+          const highlight = highlightLines;
+          const data: string = highlight.includes(applyHighlights)
+            ? 'highlight'
+            : null;
+          return { data };
+        } else {
+          return {};
+        }
+      };
+
+      return hasLang ? (
+        <SyntaxHighlighter
+          style={syntaxTheme}
+          language={hasLang[1]}
+          PreTag="div"
+          className="codeStyle"
+          showLineNumbers={true}
+          wrapLines={hasMeta}
+          useInlineStyles={true}
+          lineProps={applyHighlights}
+        >
+          {props.children}
+        </SyntaxHighlighter>
+      ) : (
+        <code className={className} {...props} />
+      )
+    },
+  }
 
 
   return (
     <div className="max-w-[50%]">
-      <Markdown remarkPlugins={[remarkGfm]}>{token}</Markdown>
+      <Markdown components={MarkdownComponents} remarkPlugins={[remarkGfm]}>{token}</Markdown>
     </div>
   )
 }
