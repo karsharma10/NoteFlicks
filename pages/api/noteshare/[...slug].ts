a085eb0af9dda9bf9470f0c4b20f68cd33aaf496
Kartik Sharma <kash5740@colorado.edu>
2024-03-02 15:15:22 -0700

    Added Get Request to Get Note with specific ID

--- pages/api/noteshare/[...slug].ts
+++ pages/api/noteshare/[...slug].ts
@@ -12,15 +12,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             const client = await clientPromise;
             const db = client.db(databaseName);
             const id = new ObjectId(slug?.toString()); //convert slug to object id
+            console.log(id);
+            const getNote = await db.collection(databaseName).find({"_id": id}).toArray();
 
-            
+            res.json(getNote);
 
         }catch (e){
             res.status(400).json({message: "failed to find note in the database"});
         }
-
-
-
+        
     } else{
         res.status(401).json({message: "API request " + method + " not allowed"});
     }
