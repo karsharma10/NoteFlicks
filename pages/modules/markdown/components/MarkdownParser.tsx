989848951230ca1338fd9818243b0292b99c0626
Pie :3! <94876844+PiCake314@users.noreply.github.com>
2024-03-02 12:48:22 -0700

    added mark lib

--- pages/modules/markdown/components/MarkdownParser.tsx
+++ pages/modules/markdown/components/MarkdownParser.tsx
@@ -1,8 +1,26 @@
+import { MarkdownState } from '../../core/components/Main';
 import { markdownParserProps } from '../types/markdownParser';
 
-export default function MarkdownParser({ token }: markdownParserProps): JSX.Element {
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
+
+
+class MarkdownElement {
+  type: MarkdownState;
+  content: string;
+  // children: MarkdownElement[];
+
+  constructor(type: MarkdownState, content: string){
+    this.type = type;
+    this.content = content;
+    // this.children = children;
+  }
+}
+
+
+export default function MarkdownParser({ token, mdState, setMdSate }: markdownParserProps): JSX.Element {
   return (
-    <h2>{ token }</h2>
+    <Markdown remarkPlugins={[remarkGfm]}>{token}</Markdown>
   )
 }
--- pages/modules/markdown/types/markdownParser.ts
+++ pages/modules/markdown/types/markdownParser.ts
@@ -1,3 +1,8 @@
+import { Dispatch, SetStateAction } from "react";
+import { MarkdownState } from "../../core/components/Main";
+
 export type markdownParserProps = {
   token: string
+  mdState: MarkdownState
+  setMdSate: Dispatch<SetStateAction<MarkdownState>>
 }
