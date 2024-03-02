b7ff9d7f6ce82cf8e76f2160628b05020d10cec8
Kartik Sharma <kash5740@colorado.edu>
2024-03-02 13:16:04 -0700

    added config folde

--- pages/api/noteshare/comment.ts
+++ pages/api/noteshare/comment.ts
@@ -1,1 +1,20 @@
 import clientPromise from "../../../lib/mongodb";
+import {NextApiRequest, NextApiResponse} from "next";
+
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+    const method = req.method;
+
+    if(method == "POST"){
+        try {
+            const client = await clientPromise;
+            const db = client.db("NoteFlixDb");
+            
+        }catch (e){
+
+        }
+    } else{
+        res.status(401).json({message: "API request " + method + " not allowed"});
+    }
+
+}
