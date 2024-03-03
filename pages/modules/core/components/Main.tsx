3333fa772780a2e6f61565e27e0b96f779cb067d
Sam Goto <osgo1115@icloud.com>
2024-03-02 18:43:07 -0700

    Init view page

--- pages/modules/core/components/Main.tsx
+++ pages/modules/core/components/Main.tsx
@@ -7,6 +7,7 @@ import { Note } from '../../../../types/noteType';
 import { SaveNoteResponse } from '../type/saveNote';
 import Modal from './Modal';
 import styles from './core.module.css';
+import { NextRouter, useRouter } from 'next/router';
 
 
 export default function Main(): React.JSX.Element {
@@ -15,7 +16,7 @@ export default function Main(): React.JSX.Element {
   const { postData, error, loading: saveNoteLoading } = usePostRequest<SaveNoteResponse>('/api/noteshare');
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [saveNoteRes, setSaveNoteRes] = useState<SaveNoteResponse | null>(null);
-
+  const router: NextRouter = useRouter();
 
   function saveNote(): void {
     if (!token) return;
@@ -42,15 +43,23 @@ export default function Main(): React.JSX.Element {
       return <h1>Error</h1>
     }
 
+    const link = `http://localhost:3000/view?noteId=${saveNoteRes?.insertedId}`
+
     return (
-      <p>{`http://localhost:3000/api/noteshare/${saveNoteRes?.insertedId}`}</p>
+     <div className={styles.modalContentContainer}>
+       <p>{link}</p>
+       <button className={styles.modalContentViewButton} onClick={() => router.push(link)}>View</button>
+     </div>
     )
   }
 
   return (
     <div className={styles.mainContainer}>
       <Header saveNote={saveNote}></Header>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} children={modalContent()}></Modal>
+      <Modal isOpen={isModalOpen}
+             onClose={() => setIsModalOpen(false)}
+             children={modalContent()}
+      ></Modal>
 
       <div className={styles.mainMarkdownContainer}>
         <MarkdownInput setToken={setToken}></MarkdownInput>
