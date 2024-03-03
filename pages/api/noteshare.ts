7646477af2f1339096d40443abdbdceea6995999
Sam Goto <osgo1115@icloud.com>
2024-03-03 01:16:39 -0700

    Improved UI

--- pages/api/noteshare.ts
+++ pages/api/noteshare.ts
@@ -2,8 +2,6 @@ import clientPromise from "../../lib/mongodb";
 import {NextApiRequest, NextApiResponse} from "next";
 import {NoteType} from "../../types/noteType";
 import {databaseName} from "../../configurations/databaseConfig";
-import { addDoc, collection } from '@firebase/firestore';
-import { db as firedb } from '../../configurations/fireabseConfig';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const method = req.method;
@@ -18,20 +16,14 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                 res.status(400).json({message: "field value is empty"});
             }
 
-            const docRef = await addDoc(collection(firedb, "documents"), {
+            const uploadNote: NoteType = { //create note type
+                title: req.body.title,
                 content: req.body.content,
-                ownerId: req.body.ownerId,
-            });
-
-            return res.status(200).json({ success: true, id: docRef.id });
-            // const uploadNote: NoteType = { //create note type
-            //     title: req.body.title,
-            //     content: req.body.content,
-            //     totalLines: req.body.totalLines,
-            //     comments: []
-            // }
-            // let post_note = await db.collection(databaseName).insertOne(uploadNote);
-            // res.json(post_note);
+                totalLines: req.body.totalLines,
+                comments: []
+            }
+            let post_note = await db.collection(databaseName).insertOne(uploadNote);
+            res.json(post_note);
 
         } catch (e) {
             res.status(400).json({message: "failed to post new note to the database"});
